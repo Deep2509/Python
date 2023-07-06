7588c6c62685502f431dbcdb31ff8211383b49c1
Deep2509 <deeptripathi50@gmail.com>
2023-07-06 09:54:53 +0530

    merge other

---
+++ Daily Tasks/Task 2.py
@@ -0,0 +1,22 @@
+
+#1. Area of Triangle
+
+B= int(input("Enter base: "))
+H= int(input("Enter Height: "))
+
+print("Area of Traingle is: ", 1/2*B*H )
+
+
+#2. Area of Circle
+
+Pie = 3.14
+r=int(input("Enter the radius: "))
+
+print("Area of Circle is: ", Pie*r*r)
+
+#3. Area of Rectangle
+
+a= int(input("Enter Length: "))
+b= int (input("Enter Breadth: "))
+
+print("Area of Rectangle is: ", a*b)
