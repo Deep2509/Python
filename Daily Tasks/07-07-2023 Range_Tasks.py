9ec6b60bb13f5c2094e47259aad83bd7804f0bf3
Deep2509 <deeptripathi50@gmail.com>
2023-07-07 10:02:52 +0530

    merge other

---
+++ Daily Tasks/07-07-2023 Range_Tasks.py
@@ -0,0 +1,74 @@
+#1. Print Hello word 10 times
+
+for i in range(0,10):
+    print("Hello")
+
+#2. print number 99 to 1
+
+for i in range(99,0,-1):
+    print(i)
+
+#3. Take 5 different name input from user
+
+for i in range(5):
+    name1=input("Enter first name: ")
+    name2=input("Enter Second name: ")
+    name3=input("Enter Third name: ")
+    name4=input("Enter Fourth name: ")
+    name5=input("Enter Fifth name: ")
+
+#4. Take 10 different input from user and find if they are odd or even
+
+for i in range(1):
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+    num=int(input("Enter num : "))
+
+    if(num%2==0):
+        print("Number is even")
+    else:
+        print("Number is odd")
+
+
+
+
+
+
